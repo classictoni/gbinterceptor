98b111a82bf1d7f4ce2fb4eee4244ef65a5a4ad8
Toni <toni@ik.me>
2026-06-01 23:13:04 +0200

    chore: make sure build doesn't change code

--- firmware/build-firmware-for-names.sh
+++ firmware/build-firmware-for-names.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+old_name=`grep 'GBI_NAME' ../usb_descriptors.h | sed "s/\"$//" | sed "s/^#define GBI_NAME.*\"//"`
+
+revert_code_changes() {
+    sed -i "s/^#define GBI_NAME.*$/#define GBI_NAME \"$old_name\"/" ../usb_descriptors.h || fail
+}
+
 fail() {
+    revert_code_changes
     printf '\nmake sure you are running this script in the firmware/build directory\n' && exit 1
 }
 
@@ -18,7 +25,8 @@ names=(
 )
 for name in "${names[@]}"; do
     sed -i "s/^#define GBI_NAME.*$/#define GBI_NAME \"$name\"/" ../usb_descriptors.h || fail
-    make
+    make || fail
     cp gb_interceptor.uf2 "gb_interceptor_$name.uf2"
     echo "done building firmware for $name"
 done
+revert_code_changes
